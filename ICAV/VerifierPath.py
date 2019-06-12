57db5865afd1a9352d63bd783e2206f64f48a41e
Morten <mitmme16@student.aau.dk>
2019-06-12 11:08:49 +0200

    fix

--- ICAV/VerifierPath.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-from os.path import expanduser
-
-home = expanduser("~")
-veri = home + '/desktop/uppaal-4.1.22/bin-Windows/verifyta.exe'
-
-#'/Downloads/uppaal-4.1.20-stratego-5/bin-Linux/verifyta'
--- README.md
+++
@@ -1,2 +0,0 @@
-# ICAV
-ICAV implementation
