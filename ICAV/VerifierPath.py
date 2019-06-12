1cb242cbfd7204006a89b87658a0f00fba64cb34
Morten <mitmme16@student.aau.dk>
2019-06-12 11:07:45 +0200

    test

--- ICAV/VerifierPath.py
+++ ICAV/VerifierPath.py
@@ -3,6 +3,6 @@ import os
 from os.path import expanduser
 
 home = expanduser("~")
-veri = home + '/Desktop/uppaal-4.1.22/bin-Windows/verifyta.exe'
+veri = home + '/desktop/uppaal-4.1.22/bin-Windows/verifyta.exe'
 
 #'/Downloads/uppaal-4.1.20-stratego-5/bin-Linux/verifyta'
